c4608c375df2f26fbcaa1d91aff4b65feb4f35a0
T. Felipe <tainan.bussiness@gmail.com>
2026-02-04 21:29:31 -0300

    Add function ContextWithTimeout

--- utils.go
+++ utils.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"time"
+
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -92,4 +94,11 @@ func EnsureIndex(ctx context.Context, collection string, indexModel mongo.IndexM
 	}
 
 	return nil
+}
+
+func ContextWithTimeout(parent context.Context, duration time.Duration) (context.Context) {
+	ctx, cancel := context.WithTimeout(parent, duration)
+	defer cancel()
+
+	return ctx
 }
